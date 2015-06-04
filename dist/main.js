4d883c4a854e5352fbbd740bdcf6e100c266fcf3
Ramona Bellamy <bellamy2727@yahoo.com>
2015-06-04 15:34:08 -0500

    added user and message

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 	
+	var input;
+	var user;
+
 	$('#formbox').submit(function (e){
 		e.preventDefault();
-		var input = $('#inputbox').val();
-		console.log(input);
+		input = $('#inputbox').val();
+		user = $('#usernamebox').val();
 
-		$.post('http://tiny-pizza-server.herokuapp.com/collections/mona', {message: input}, 'JSON' );
+		console.log(input);
+		console.log(user);
+		$.post('http://tiny-pizza-server.herokuapp.com/collections/mona', {username: user, message: input, }, 'JSON' );
+
 	});
 
 })
